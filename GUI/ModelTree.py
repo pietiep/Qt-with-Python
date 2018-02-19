2484eaa9150ae85d8b8aa127f3fd29d493b851d5
pietiep <peter.protassow@uni-bielefeld.de>
2018-02-19 15:52:44 +0100

    new Node method from mctdh.so implimented

--- GUI/ModelTree.py
+++ GUI/ModelTree.py
@@ -5,13 +5,17 @@ class ModelTree(object):
         self.config_file = config_file
         self.sys_file = sys_file
         self.bottom_list = []
+        self.mode_list =[]
         self.layer_list = []
         self.layer_matr = []
+        self.lay_matr_mode = []
         self.config = mctdh.controlParameters()
         self.config.initialize(self.config_file)
         self.basis = mctdh.MctdhBasis()
         self.basis.initialize(self.sys_file, self.config)
         self.node = mctdh.MctdhNode()
+        self.phys = mctdh.PhysCoor()
+
     def getBottomlayer(self):
         """Get the bottom nodes"""
         for i in range(self.basis.NmctdhNodes()):
@@ -19,6 +23,15 @@ class ModelTree(object):
             if self.node.Bottomlayer() == True:
                 self.bottom_list.append(i)
         return self.bottom_list #List of the i-th bottom node
+
+    def getPhysCoord(self):
+        """get the Modes of the pys. Coordinates"""
+        for i in range(self.basis.NmctdhNodes()):
+            self.node = self.basis.MCTDHnode(i)
+            if self.node.Bottomlayer() == True:
+                self.phys = self.node.phys_coor()
+                self.mode_list.append(self.phys.mode()) #append Modes to list
+
     def nlayer(self, i):
         """Recursion function that goes from the i-th node up to the top layer of the tree"""
         self.node = self.basis.MCTDHnode(i) #i-th Node
@@ -28,12 +41,22 @@ class ModelTree(object):
         new_list = list(self.layer_list) #copy instead of reference
         del self.layer_list[:]
         return new_list
+
     def getLayerMatr(self):
         """Takes all bottom nodes and returns for each a list of the path to the top node"""
         self.layer_matr = [list(reversed(self.nlayer(b_))) for b_ in self.getBottomlayer()]
+
+    def modeToGetLayer(self):
+        """Combines layer_matr with mode_list"""
+        self.lay_matr_mode = [(l_.append(self.mode_list)) for i,l_ in enumerate(self.layer_matr)]
+
 
 if __name__ == '__main__':
 
     model = ModelTree()
     model.getLayerMatr()
+    model.getPhysCoord()
+#    model.modeToGetLayer()
+    print model.mode_list
     print model.layer_matr
+    print model.lay_matr_mode
