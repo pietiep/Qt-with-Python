610d122b609454927d642f6cb36a6943dabfa527
pietiep <33902724+pietiep@users.noreply.github.com>
2018-04-23 15:02:08 +0200

    Add files via upload

---
+++ data/NO3/states.f
@@ -0,0 +1,9 @@
+!   nstat:   number of electronic states
+!   qn:      number of coordinates
+!   nci:     number of CI data
+!   ntot:    total length of input data per point
+      integer nstat, qn, qn2
+      integer nci, ntot
+!      parameter (nstat=5, qn=7, nci=25, ntot=nstat+nci, qn2=qn*qn)
+      parameter (nstat=5, qn=7, nci=0, ntot=nstat+nci, qn2=qn*qn)
+
