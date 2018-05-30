4935edee2e034575c0277b12443493c872b2af67
pietiep <peter.protassow@uni-bielefeld.de>
2018-05-30 14:49:41 +0200

    mctdh test file

---
+++ GUI/mctdhTest.py
@@ -0,0 +1,42 @@
+import mctdh
+
+config = mctdh.controlParameters()
+config.initialize('/home/piet/Schreibtisch/masterarbeit/Qt-with-Python/GUI/Projects/pro1/ch3/mctdh.config')
+basis = mctdh.MctdhBasis()
+basis.initialize('/home/piet/Schreibtisch/masterarbeit/Qt-with-Python/GUI/Projects/pro1/ch3/CH3f11.txt', config)
+
+
+bottom_list = []
+mode_list = []
+nodes_spf = {}
+
+def getBottomlayer():
+    """Get the bottom nodes"""
+    for i in range(basis.NmctdhNodes()):
+        node = basis.MCTDHnode(i)
+        if node.Bottomlayer() == True:
+            bottom_list.append(i)
+    return bottom_list
+
+print getBottomlayer()
+
+def getPhysCoord():
+    """get the Modes of the pys. Coordinates"""
+    for i in range(basis.NmctdhNodes()):
+        node = basis.MCTDHnode(i)
+        if node.Bottomlayer() == True:
+            phys = node.phys_coor()
+            mode_list.append(phys.mode()) #append Modes to list
+    return mode_list
+
+print getPhysCoord()
+
+def get_SPFs(basis):
+    """get the SPFs of each Node"""
+    for i in range(basis.NmctdhNodes()):
+        node = basis.MCTDHnode(i)
+        tdim = node.t_dim()
+        nodes_spf[i] = tdim.GetnTensor() #dict
+    return nodes_spf
+
+print get_SPFs(basis)
